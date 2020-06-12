3b451c99090e8303307d0e512a5727dad5277911
divyesh <skdivyesh333@gmail.com>
2020-06-12 19:29:22 +0530

    added phone number and email using card nad ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -36,6 +39,56 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.5,
                 ),
+              ),
+              SizedBox(
+                height: 10,
+                width: 250,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal[800],
+                      size: 30,
+                    ),
+                  title: Text(
+                      '+91 9924551540',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 16,
+                      ),
+                    ),
+                ),
+                
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                      Icons.email,
+                      color: Colors.teal[800],
+                      size: 30,
+                    ),
+                    title: Text(
+                      'skdivyesh333@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 16,
+                      ),
+                ), 
+              ),
               ),
             ],
           ),
